8635f927546c77bd900e543507e881a8a001cfa2
GabrielRiart <gabogonzalez.py@gmail.com>
2025-08-25 03:20:40 +0000

    Copilacion de todos los codigos

--- SRC/Cap3/Listing3.6.c
+++ SRC/Cap3/Listing3.6.c
@@ -1,18 +1,26 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
-int main ()
-{
- pid_t child_pid;
- /* Create a child process. */
- child_pid = fork ();
- if (child_pid > 0) {
- /* This is the parent process. Sleep for a minute. */
- sleep (60);
- }
- else {
- /* This is the child process. Exit immediately. */
- exit (0);
- }
- return 0;
-} 
+
+int main() {
+    pid_t child_pid;
+
+    /* Creamos el proceso hijo. */
+    child_pid = fork();
+
+    if (child_pid > 0) {
+        // El padre se duerme por 60 segundos.
+        sleep(60);
+                /* Este es el proceso padre. */
+        printf("Este es el proceso padre con PID %d\n", (int)getpid());
+        printf("Mi hijo tiene el PID %d\n", (int)child_pid);
+    } else {
+        /* Este es el proceso hijo. */
+        printf("Este es el proceso hijo con PID %d\n", (int)getpid()); 
+        // El hijo termina inmediatamente.
+        exit(0);
+    }
+
+    return 0;
+}
